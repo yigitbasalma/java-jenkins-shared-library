ddb054b9393d7d42bbf5989277b6d62cf3f17c96
Yigit Can Basalma <yigit.basalma@warewave.tech>
2023-02-24 01:33:04 +0300

    Fix

--- vars/lib_versionController.groovy
+++ vars/lib_versionController.groovy
@@ -1,14 +1,14 @@
 def call(Map config) {
     // Configure project version if not configured
     if ( ! config.project_version ) {
-        config.project_version = sh(
+        /*config.project_version = sh(
             script: "${config.script_base}/metadata/metadata.py -p ${config.b_config.project.name} -a ${config.b_config.project.name} -e ${config.environment} get --project-version",
             returnStdout: true
-        ).trim()
+        ).trim()*/
         def full_version = sh(
-            script: "cat ${config.config_file} | python3 -c 'import sys, yaml; print(\".\".join(yaml.load(sys.stdin, Loader=yaml.CLoader)[\"project\"][\"version\"].split(\".\")[:2]))'",
+            script: "cat ${config.config_file} | python3 -c 'import sys, yaml; print(\".\".join(yaml.load(sys.stdin, Loader=yaml.CLoader)[\"project\"][\"version\"]))'",
             returnStdout: true
         ).trim()
-        config.project_full_version = "${full_version}.${config.project_version}"
+        config.project_full_version = full_version
     }
 }
