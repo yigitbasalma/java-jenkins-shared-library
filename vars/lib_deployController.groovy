7996ea993e48a9c972b7af5d9e2142ed121c95f7
Yiğit Can Başalma <yigit.basalma@local>
2023-09-04 15:22:28 +0300

    Fix

--- vars/lib_deployController.groovy
+++ vars/lib_deployController.groovy
@@ -81,7 +81,7 @@ def nativeDocker(Map config, String image, Map r_config, String containerReposit
     sshagent(credentials: [config.remoteHostCredentialID]) {
       sh """
       #!/bin/bash
-      ssh -o StrictHostKeyChecking=no ${config.remoteUser}@${config.remoteHost} << EOF
+      ssh -tt -o StrictHostKeyChecking=no -p ${config.remoteHostSSHPort} ${config.remoteUser}@${config.remoteHost} << EOF
       docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=${r_config.name} --format="{{.ID}}")) 2>&1 /dev/null
       docker run -d --name ${r_config.name} ${dockerArgs.unique().join(" ")} ${image}
       exit 0
