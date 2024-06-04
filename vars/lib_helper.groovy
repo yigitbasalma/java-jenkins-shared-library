5624be691278b60896b536ad30da42cbbbd706e4
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-06-04 22:47:07 +0300

    test

--- vars/lib_helper.groovy
+++ vars/lib_helper.groovy
@@ -42,10 +42,14 @@ def configureInit(Map config) {
         "devops-preprod",
     ]
 
-    if (config.github_hook && env.REF.contains('refs/heads/')) {
-        def branchName = env.REF.replaceFirst('^refs/heads/', '')
-        sh "echo 'Branch name: ${branchName}'"
-        env.REF = branchName
+    if (env.REF) {
+        // Manipulate the env.REF to strip 'refs/heads/' prefix if it exists
+        if (config.github_hook && env.REF.contains('refs/heads/')) {
+            def branchName = env.REF.replaceFirst('^refs/heads/', '')
+            echo "Branch name: ${branchName}"
+            env.REF = branchName
+        }
+        config.target_branch = env.REF
     }
 }
 
