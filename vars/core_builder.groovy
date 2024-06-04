cf85551d42c170b59e0b4d03843d74c3eea0f464
Ferhat Kacar <ferhatkacar.partner@kafein.com.tr>
2024-06-04 23:09:19 +0300

    rename jobs when starts with ref/heads/

--- vars/core_builder.groovy
+++ vars/core_builder.groovy
@@ -76,6 +76,11 @@ def call(Map config) {
                             returnStdout: true
                         ).trim()
 
+                        // Manipulate the job name if starts with ref/heads(generic_webhook)
+                        if (config.job_name.startsWith("refs/heads/")) {
+                            config.job_name = config.job_name.replaceFirst('^refs/heads/', '')
+                        }
+
                         // Configure commit ID for project
                         commitID = sh(
                             script: """
--- vars/core_deployer.groovy
+++ vars/core_deployer.groovy
@@ -53,6 +53,11 @@ def call(Map config) {
                             returnStdout: true
                         ).trim()
 
+                        // Manipulate the job name if starts with ref/heads(generic_webhook)
+                        if (config.job_name.startsWith("refs/heads/")) {
+                            config.job_name = config.job_name.replaceFirst('^refs/heads/', '')
+                        }
+
                         // Configure image from params
                         if ( params.containsKey("IMAGE") && params.IMAGE != "" ) {
                             config.image = params.IMAGE
